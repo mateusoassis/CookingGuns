1653c7e843c8543e4ca9c57bf4022919192d1242
mateusoassis <mateusoassis@gmail.com>
2022-03-07 16:46:23 -0300

    vamo que vamo

---
+++ Assets/Scripts/_PlayerStats.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PlayerStats", menuName = "Cooking Guns/PlayerStats", order = 0)]
+public class _PlayerStats : ScriptableObject
+{
+    public int playerHealth;
+    public int currency1;
+    public int currency2;
+    public int currency3;
+}
