e473f30705f71c59ee0c9c23aecdc4a86ddf06a9
RodrigoALemos <rodrigoaraujolemos@gmail.com>
2022-03-14 12:07:11 -0300

    Vou refazer tudo, relaxe

--- Assets/Scripts/Crafting Scripts/CraftableItem.cs
+++ Assets/Scripts/Crafting Scripts/CraftableItem.cs
@@ -2,13 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public struct ItemAmount{
+    public GameObject item;
+    [Range(1, 4)]
+    public int amount;
+}
+
+
 
 [CreateAssetMenu(fileName = "CraftableItem", menuName = "Cooking Guns/CraftableItem", order = 1)]
 public class CraftableItem : ScriptableObject
 {
-    public string[]  Recipe = new string[4];
-    public string Name;
-    public Sprite Img;
+    public List<ItemAmount> Materials;
+    public List<ItemAmount> Results;
 
+    public bool CanCraft(){
+        return false;
+    }
+    public void Craft(){
+        
+    }
 
 }
