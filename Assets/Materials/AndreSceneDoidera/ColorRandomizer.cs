2f595a0ec22948ed6b67880967c6cb5b35ab9ab3
mateusoassis <mateusoassis@gmail.com>
2022-05-07 16:50:42 -0300

    feat: randomizador de cores nos macaroons da cena de andré

---
+++ Assets/Materials/AndreSceneDoidera/ColorRandomizer.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorRandomizer : MonoBehaviour
+{
+    [SerializeField] private Material[] foraMaterial;
+    [SerializeField] private Material[] recheioMaterial;
+
+    [SerializeField] private MeshRenderer[] foraObjects;
+    [SerializeField] private MeshRenderer[] recheioObjects;
+
+    private int foraRandomizer;
+    private int recheioRandomizer;
+
+    void Start()
+    {
+        for(int i = 0; i < foraObjects.Length; i++)
+        {
+            foraRandomizer = Random.Range(0, foraMaterial.Length);
+            foraObjects[i].material = foraMaterial[foraRandomizer];
+        }
+        for(int i = 0; i < recheioObjects.Length; i++)
+        {
+            recheioRandomizer = Random.Range(0, recheioMaterial.Length);
+            recheioObjects[i].material = recheioMaterial[recheioRandomizer];
+        }
+    }
+}
