a31fe5ab7acdd4aa27a03fefe424e5f2974aa98b
mateusoassis <mateusoassis@gmail.com>
2022-05-08 11:08:57 -0300

    fix: andré definitivamente vai jogar isso pela janela LUL

--- Assets/Scripts/Player/_WeaponNaming.cs
+++ Assets/Scripts/Player/_WeaponNaming.cs
@@ -4,27 +4,37 @@ using UnityEngine;
 
 public class _WeaponNaming : MonoBehaviour
 {
+    [Header("Cores/qualidade de nomes de armas")]
+    public Color rareColor;
+    public Color epicColor;
+    public Color uniqueColor;
+    public Color legendaryColor;
+
     [Header("Pistolas")]
     public string[] pistolPrefixoDe;
     public string[] pistolSufixoDe;
     public string[] pistolPrefixo;
     public string[] pistolSufixo;
+    public string[] pistolUniqueName;
 
     [Header("Shotguns")]
     public string[] shotgunPrefixoDe;
     public string[] shotgunSufixoDe;
     public string[] shotgunPrefixo;
     public string[] shotgunSufixo;
+    public string[] shotgunUniqueName;
 
     [Header("Machine Guns")]
     public string[] machineGunPrefixoDe;
     public string[] machineGunSufixoDe;
     public string[] machineGunPrefixo;
     public string[] machineGunSufixo;
+    public string[] machineGunUniqueName;
 
     [Header("Grenade Launchers")]
     public string[] grenadeLauncherPrefixoDe;
     public string[] grenadeLauncherSufixoDe;
     public string[] grenadeLauncherPrefixo;
     public string[] grenadeLauncherSufixo;
+    public string[] grenadeLauncherUniqueName;
 }
