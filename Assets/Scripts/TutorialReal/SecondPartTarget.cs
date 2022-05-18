f90ca65926d58681d9a8753fa07195677b3468db
mateusoassis <mateusoassis@gmail.com>
2022-05-18 17:59:26 -0300

    fix: script nas gosminhas só ativar no tutorial

--- Assets/Scripts/TutorialReal/SecondPartTarget.cs
+++ Assets/Scripts/TutorialReal/SecondPartTarget.cs
@@ -7,14 +7,20 @@ public class SecondPartTarget : MonoBehaviour
 {
     public SecondPartOpenDoor secondPartOpenDoor;
     public WindowContainer windowContainer;
+    public int scene;
 
     public int type;
     // 0 = pudim
     // 1 = shieldoca
 
+    void Awake()
+    {
+        scene = SceneManager.GetActiveScene().buildIndex;
+    }
+
     void Start()
     {
-        if(SceneManager.GetActiveScene().buildIndex == 3)
+        if(scene == 3)
         {
             secondPartOpenDoor = GameObject.Find("2nd_PartColliders").GetComponent<SecondPartOpenDoor>();
             windowContainer = GameObject.Find("TutorialWindowContainer").GetComponent<WindowContainer>();
@@ -23,12 +29,12 @@ public class SecondPartTarget : MonoBehaviour
 
     void OnDestroy()
     {
-        if(type == 0)
+        if(type == 0 && scene == 3)
         {
             secondPartOpenDoor.pudim = true;
             windowContainer.NextDialogue();
         }
-        else if(type == 1)
+        else if(type == 1 && scene == 3)
         {
             secondPartOpenDoor.shieldoca = true;
             windowContainer.NextDialogue();
