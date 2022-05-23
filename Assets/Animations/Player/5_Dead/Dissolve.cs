378b422eafba9a7730999833fd4feb5882b0882e
mateusoassis <mateusoassis@gmail.com>
2022-05-23 04:25:24 -0300

    fix: uns treco aí e teste do dissolve mas deu ruim lul

--- Assets/Animations/Player/5_Dead/Dissolve.cs
+++ Assets/Animations/Player/5_Dead/Dissolve.cs
@@ -5,18 +5,24 @@ using UnityEngine;
 public class Dissolve : MonoBehaviour
 {
     public float delayToDissolve;
-    public Material dissolveMaterial;
-    public SkinnedMeshRenderer[] playerRenderers;
+    public float dissolveSpeed;
+    public float delayTimer;
 
-    public Material[] oldMaterials;
+    public Material[] dissolveMaterialsCorrectNow;
 
     void Awake()
     {
-        oldMaterials = new Material[7];
+        //oldMaterials = new Material[7];
+        for(int i = 0; i < dissolveMaterialsCorrectNow.Length; i++)
+        {
+            dissolveMaterialsCorrectNow[i].SetFloat("AlphaControl_", -1f);
+        }
     }
 
     void Start()
     {
+        Debug.Log(dissolveMaterialsCorrectNow[0].GetFloat("AlphaControl_"));
+        /*
         for(int i = 0; i < playerRenderers.Length; i++)
         {
             for(int n = 0; n< playerRenderers.Length; n++)
@@ -26,14 +32,31 @@ public class Dissolve : MonoBehaviour
         }
 
         StartCoroutine(DissolveNow());
+        */
     }
-
+    
+    /*
     public IEnumerator DissolveNow()
     {
         yield return new WaitForSeconds(delayToDissolve);
         foreach(SkinnedMeshRenderer k in playerRenderers)
         {
             k.GetComponent<SkinnedMeshRenderer>().material = dissolveMaterial;
+        }
+    }
+    */
+
+    void Update()
+    {
+        delayTimer += Time.deltaTime;
+
+        if(delayTimer > delayToDissolve)
+        {
+            for(int i = 0; i < dissolveMaterialsCorrectNow.Length; i++)
+            {
+                dissolveMaterialsCorrectNow[i].SetFloat("AlphaControl_", Mathf.MoveTowards(dissolveMaterialsCorrectNow[i].GetFloat("AlphaControl_"), 1f, Time.deltaTime * dissolveSpeed));
+            }
+            Debug.Log(dissolveMaterialsCorrectNow[0].GetFloat("AlphaControl_"));
         }
     }
 }
