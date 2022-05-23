5e3208d538e924afecbd65cfa163451b2de7929c
mateusoassis <mateusoassis@gmail.com>
2022-05-23 00:05:52 -0300

    fix: coisas novas de UI e tentando fazer a cena conceitual funcionar

--- Assets/Scripts/Camera/CinemachineSwitchBlend.cs
+++ Assets/Scripts/Camera/CinemachineSwitchBlend.cs
@@ -6,9 +6,12 @@ using Cinemachine;
 
 public class CinemachineSwitchBlend : MonoBehaviour
 {
+    public bool conceptualCameraBool;
     [SerializeField] private CinemachineVirtualCamera mainCamera;
     [SerializeField] private CinemachineVirtualCamera petCamera;
+    [SerializeField] private CinemachineVirtualCamera conceptualCamera;
     private bool mainCameraMode;
+    private bool conceptualMode;
 
     [Header("Inserir mesma duração das trocas")]
     public float mainToPetDuration;
@@ -24,18 +27,47 @@ public class CinemachineSwitchBlend : MonoBehaviour
         SwitchPriority();
     }
 
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.J) && conceptualCameraBool)
+        {
+            UnityEngine.Debug.Log("troca");
+            SwitchToConceptual();
+        }
+    }
+
     public void SwitchPriority()
     {
         if(mainCameraMode)
         {
             mainCamera.Priority = 0;
             petCamera.Priority = 10;
+            conceptualCamera.Priority = 0;
         }
         else
         {
             mainCamera.Priority = 10;
             petCamera.Priority = 0;
+            conceptualCamera.Priority = 0;
         }
         mainCameraMode = !mainCameraMode;
+    }
+
+    public void SwitchToConceptual()
+    {
+        if(conceptualMode)
+        {
+            mainCamera.Priority = 0;
+            petCamera.Priority = 0;
+            conceptualCamera.Priority = 10;
+        }
+        else
+        {
+            mainCamera.Priority = 10;
+            petCamera.Priority = 0;
+            conceptualCamera.Priority = 0;
+        }
+        conceptualMode = !conceptualMode;
+        UnityEngine.Debug.Log("trocou");
     }
 }
